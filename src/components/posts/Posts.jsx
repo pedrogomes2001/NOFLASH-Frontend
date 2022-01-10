0249ec67dd0443f594d1cf273a73d594a90de1f1
pedrogomes2001 <pmlg2001@gmail.com>
2022-01-10 22:07:46 +0000

    first website page

---
+++ src/components/posts/Posts.jsx
@@ -0,0 +1,15 @@
+import Post from "../post/Post";
+import "./posts.css";
+
+export default function Posts() {
+  return (
+    <div className="posts">
+      <Post img="https://www.pexels.com/pt-br/foto/vista-aerea-da-cidade-2246476/" />
+      <Post img="" />
+      <Post img=""/>
+      <Post img=""/>
+      <Post img=""/>
+      <Post img=""/>
+    </div>
+  );
+}
